77036f55bfe8709279872ae7bf7363e723163348
elimS2 <elims2@gmail.com>
2020-01-08 23:06:16 +0000

    commit 2

---
+++ create-new-repository-example.sh
@@ -0,0 +1,7 @@
+#…or create a new repository on the command line:
+echo "# hillel-php-advanced" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/elimS2/hillel-php-advanced.git
+git push -u origin master
