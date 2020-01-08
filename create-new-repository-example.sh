df3fdddc7f7d8375ae120f2fc041490960bf8161
elimS2 <elims2@gmail.com>
2020-01-08 23:48:17 +0000

    commit 4

--- create-new-repository-example.sh
+++ create-new-repository-example.sh
@@ -5,3 +5,7 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/elimS2/hillel-php-advanced.git
 git push -u origin master
+
+git add create-new-repository-example.sh
+git commit -m "commit 2"
+git push -u origin master
